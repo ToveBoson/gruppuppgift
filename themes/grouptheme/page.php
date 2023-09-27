0c9278a1895bfd452d1f1f13d693624b1b16feac
Ida Danielsson <ida.danielsson96@icloud.com>
2023-09-27 16:43:10 +0200

    added styling

--- themes/grouptheme/page.php
+++ themes/grouptheme/page.php
@@ -3,11 +3,14 @@
 <?php get_header(); ?>
 
 <div class="page-content">
+    <div class="heading-container">
+    <h1 class="page-heading"><?php wp_title(""); ?></h1>
+    </div>
     <?php 
     if (have_posts()) :
         while (have_posts()) : the_post();
             if (is_checkout()) { // Kontrollera om det är kassasidan.
-                // Visa inloggnings- och registreringsformulär med länkar för att logga in och skapa konto.
+                // Visa inloggnings- och registreringsformulär med länkar för att logga in.
                 echo do_shortcode('[woocommerce_my_account login_text="Logga in" register_text="Skapa konto"]');
             }
             ?>
