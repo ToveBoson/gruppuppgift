31a31453af1ec7c65fd61d8c9777865b178c5584
Ida Danielsson <ida.danielsson96@icloud.com>
2023-10-03 14:02:48 +0200

    styling

--- themes/grouptheme/single.php
+++ themes/grouptheme/single.php
@@ -1,16 +1,29 @@
 <?php get_header(); ?>
 
-<div class="news-wrapper">
-    <div class="news-page">
+<div class="single-wrapper">
+    <div class="title-container">
+        <h2><?php wp_title("");?></h2>
+    </div>
+    <div class="single-page">
+    
+    
+    
     <?php
     if (have_posts()) :
         while (have_posts()) : the_post();
             ?>
+            
             <article class="single-news"> 
-                <h1 class="single-title"><?php the_title(); ?></h1>
+                
                 <div class="single-content">
-                    <p class="news-text"><?php the_content(); ?></p>
-                </div>
+                    <p class="news-text">
+                        <?php the_content(); ?>
+                    </p>
+                    </div>
+                    <div class="single-news-img">
+                        <?php the_post_thumbnail(); ?>
+                    </div>
+                
             </article>
             <?php
         endwhile;
