76f4567c7575022206c931df6f8fb0b7b13ef0ac
Ida Danielsson <ida.danielsson96@icloud.com>
2023-10-10 13:30:24 +0200

    last fixes

--- themes/grouptheme/single.php
+++ themes/grouptheme/single.php
@@ -1,27 +1,50 @@
 <?php get_header(); ?>
 
 <div class="single-wrapper">
+    <div class="single-page">
+        <?php 
+            if (is_singular('product')) {
+        ?>
     <div class="title-container">
         <h2><?php the_title();?></h2>
     </div>
-    <div class="single-page">
+    <?php
+        }
+        ?>
         <?php
         if (have_posts()) :
             while (have_posts()) : the_post();
                 ?>
                 <article class="single-news"> 
                     <div class="single-content">
-                        <p class="news-text">
-                
+                    <?php
+                    if(has_category("Nyheter")) {
+                        ?>
+                        <h2 class="single-news-heading"><?php the_title(); ?></h2>
+                    <?php    
+                    }
+                    ?>
                   <div class="news-content-container">
-                    
                                 <?php the_content(); ?>
                             </div>
                         </p>
                     </div>
-                    <div class="single-news-img">
-                        <?php the_post_thumbnail(); ?>
-                    </div>
+                    <?php
+                   $thumbnail_class = ''; // Skapa en variabel för klassnamnet och sätt den som standard till en tom sträng
+
+                   if (has_category('Nyheter')) {
+                       // Om det är en sida med kategorin "Nyheter"
+                       $thumbnail_class = 'single-news-img'; // Ange klassnamnet för nyhetsinlägg
+                   } elseif (is_singular('stores')) {
+                       // Om det är en enskild butikssida
+                       $thumbnail_class = 'store-thumbnail'; // Ange klassnamnet för butikssidor
+                   }
+                   ?>
+                   
+                   <div class="<?php echo $thumbnail_class; ?>">
+                       <?php the_post_thumbnail(); ?>
+                   </div>
+                 
                 </article>
                 <?php
             endwhile;
